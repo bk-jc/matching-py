bcfbf1e2396e11a7b18934cfdcf18988d74820ac
bas <bas.krahmer@talentflyxpert.com>
2023-07-10 15:52:07 +0200

    Expand synthetic data to include a wider skill range

--- src/data.py
+++ src/data.py
@@ -1,4 +1,5 @@
 import json
+from copy import deepcopy
 
 import torch
 from datasets import Dataset
@@ -9,8 +10,18 @@ def get_data(a, data_filepath):
     data = []
 
     if 'sample' in data_filepath:
-        data.append(json.load(open(data_filepath)))
+        sample = json.load(open(data_filepath))
+        data.append(sample)
         data = data * a.val_steps * a.train_batch_size
+
+        # Synthetically create various numbers of skills
+        skill_freqs = {i: a.train_batch_size for i in range(5, 8)}
+        for n_skills, freq in skill_freqs.items():
+            n_skills_sample = deepcopy(sample)
+            n_skills_sample["cv"]["skills"] = ["Python"] * n_skills
+            n_skills_sample["job"]["skills"] = ["Python"] * n_skills
+            data += [n_skills_sample] * freq
+
     else:
         with open(data_filepath, 'r') as file:
             for line in file:
