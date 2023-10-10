4075f1b47461701fb760b47ccb908b419eb645d7
bas <bas.krahmer@talentflyxpert.com>
2023-10-10 13:44:52 +0200

    Add Optuna test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -38,3 +38,18 @@ def test_train_cross_val(tmpdir):
 
     kfold_files = [f for f in os.listdir(out_dir) if f.startswith("kfold")]
     assert len(kfold_files) == 1
+
+
+def test_optuna(tmpdir):
+    optuna_config_path = Path(__file__).parent.parent / 'config' / 'optuna_config.yaml'
+    main(parse_args([
+        "--save_path", str(tmpdir),
+        "--optuna_path", str(optuna_config_path),
+        *SHARED_ARGS
+    ]))
+    out_dir = os.path.join(tmpdir, parse_args([]).exp_name)
+    assert len(os.listdir(out_dir)) == 1  # Only one output experiment
+    out_time = os.listdir(out_dir)[0]
+    out_dir = os.path.join(out_dir, out_time)
+
+    assert len(os.listdir(out_dir)) == yaml.safe_load(open(optuna_config_path))["n_runs"]
