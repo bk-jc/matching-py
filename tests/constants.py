e1421e12f26e1ee2b0ea4bdcc8b0c4a11169d85d
bas <bas.krahmer@talentflyxpert.com>
2023-10-31 16:19:16 +0100

    Do not cache embeddings by default

--- tests/constants.py
+++ tests/constants.py
@@ -1,1 +1,2 @@
-SHARED_ARGS = ["--train_steps", "2", "--val_steps", "1", "--train_batch_size", "2", "--val_batch_size", "2"]
+SHARED_ARGS = ["--train_steps", "2", "--val_steps", "1", "--train_batch_size", "2", "--val_batch_size", "2",
+               "--cache_embeddings", "false"]
