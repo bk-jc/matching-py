0c61089fb7743d3ec9ca52a5e2c9ccd779b07d2b
bas <bas.krahmer@talentflyxpert.com>
2023-11-17 11:11:39 +0100

    Refactoring, support jobtitle embedding

--- jarvis2/modeling/jarvis.py
+++ jarvis2/modeling/jarvis.py
@@ -31,14 +31,14 @@ class Jarvis(nn.Module):
         self.num_heads = a.num_heads
         self.cache_embeddings = a.cache_embeddings
         self.skill_prefix = a.skill_prefix
-
-        skill_attention_config = copy.deepcopy(self.base_model.config)
-        setattr(skill_attention_config, "hidden_size", a.hidden_dim)
-        skill_attention_config.position_embedding_type = None  # Disable pos embeddings
-        skill_attention_config.num_attention_heads = a.num_heads
+        self.use_jobtitle = a.use_jobtitle
 
         if a.pooling_mode == "cls":
-            # TODO configurable config.num_attention_heads config.intermediate_size config.
+            skill_attention_config = copy.deepcopy(self.base_model.config)
+            setattr(skill_attention_config, "hidden_size", a.hidden_dim)
+            skill_attention_config.position_embedding_type = None  # Disable pos embeddings
+            skill_attention_config.num_attention_heads = a.num_heads
+
             self.skill_attention = BertLayer(skill_attention_config).attention
             self.skill_pooling = torch.nn.Parameter(torch.Tensor(a.hidden_dim))
             torch.nn.init.uniform_(
@@ -90,67 +90,86 @@ class Jarvis(nn.Module):
             relu_on_last_layer=readout,
         )
 
-    def get_document_embedding(self, documents, ffn_emb, ffn_readout):
+    def get_document_embedding(self, documents, ffn_emb, ffn_readout, use_jobtitle=False):
 
-        document_tensor, attention_mask = [], []
-        for doc in documents:
-            tensor, attn_mask = self.get_skill_embeddings(doc)
-            document_tensor.append(tensor)
-            attention_mask.append(attn_mask)
-        document_tensor, attention_mask = torch.stack(document_tensor), torch.stack(attention_mask)
-
+        document_tensor, attention_mask = self.get_tensors_and_masks(documents, use_jobtitle)
         batch_size, n_skills = document_tensor.shape[:2]
 
-        # Dense layer
+        # Dense layer with reshaping for skills
         document_tensor = document_tensor.view(batch_size * n_skills, -1)
         document_tensor = ffn_emb(document_tensor)
         document_tensor = document_tensor.view(batch_size, n_skills, -1)
 
-        if self.pooling_mode == "cls":
-
-            # Add skill pooling token
-            pooling = self.skill_pooling.view(1, 1, -1).repeat(len(documents), 1, 1)
-            document_tensor = torch.concat([pooling, document_tensor], dim=1)
-            attention_mask = torch.concat([torch.ones(len(documents), 1), attention_mask], dim=1)
-            attention_mask = attention_mask.view(len(documents), 1, 1, -1)
-
-            # Get skill interactions via BERT layer (output type is Tuple; take first index to get the Tensor)
-            skill_interactions = self.skill_attention(document_tensor, attention_mask=attention_mask)[0]
-
-            # Pool skill interactions (CLS token)
-            document_tensor = skill_interactions[:, 0]
-
-        elif self.pooling_mode == "max":
-
-            mask = attention_mask.unsqueeze(-1).repeat(1, 1, document_tensor.shape[-1])
-            document_tensor = torch.where(
-                mask == 0,
-                torch.tensor(-torch.inf).to(document_tensor.device), document_tensor
-            )
-            document_tensor = torch.max(document_tensor, axis=1).values
-
-        elif self.pooling_mode == "mean":
-
-            mask = attention_mask.unsqueeze(-1).repeat(1, 1, document_tensor.shape[-1])
-            document_tensor = document_tensor * mask
-
-            # Compute sum over the sequence_length axis
-            sum_pooled = document_tensor.sum(dim=1)
-
-            # Compute the count of non-zero mask values for each position
-            non_zero_counts = attention_mask.sum(dim=1, keepdim=True).clamp_min(1e-12)  # Avoid division by zero
-            document_tensor = sum_pooled / non_zero_counts
+        # Pooling out skill dimension
+        document_tensor = self.pool_skills(attention_mask, document_tensor, documents)
 
         # Dense layer
         document_tensor = ffn_readout(document_tensor)
 
         return document_tensor
 
-    def get_skill_embeddings(self, document):
+    def get_tensors_and_masks(self, documents, use_jobtitle):
+        document_tensor, attention_mask = [], []
+        for doc in documents:
+            tensor, attn_mask = self.get_skill_embeddings(doc, use_jobtitle)
+            document_tensor.append(tensor)
+            attention_mask.append(attn_mask)
+        return torch.stack(document_tensor), torch.stack(attention_mask)
+
+    def pool_skills(self, attention_mask, document_tensor, documents):
+        if self.pooling_mode == "cls":
+            document_tensor = self.cls_pooling(attention_mask, document_tensor, documents)
+        elif self.pooling_mode == "max":
+            document_tensor = self.max_pooling(attention_mask, document_tensor)
+        elif self.pooling_mode == "mean":
+            document_tensor = self.mean_pooling(attention_mask, document_tensor)
+        return document_tensor
+
+    def cls_pooling(self, attention_mask, document_tensor, documents):
+
+        # Add skill pooling token
+        pooling = self.skill_pooling.view(1, 1, -1).repeat(len(documents), 1, 1)
+        document_tensor = torch.concat([pooling, document_tensor], dim=1)
+        attention_mask = torch.concat([torch.ones(len(documents), 1), attention_mask], dim=1)
+        attention_mask = attention_mask.view(len(documents), 1, 1, -1)
+
+        # Get skill interactions via BERT layer (output type is Tuple; take first index to get the Tensor)
+        skill_interactions = self.skill_attention(document_tensor, attention_mask=attention_mask)[0]
+
+        # Pool skill interactions (CLS token)
+        document_tensor = skill_interactions[:, 0]
+        return document_tensor
+
+    @staticmethod
+    def max_pooling(attention_mask, document_tensor):
+        mask = attention_mask.unsqueeze(-1).repeat(1, 1, document_tensor.shape[-1])
+        document_tensor = torch.where(
+            mask == 0,
+            torch.tensor(-torch.inf).to(document_tensor.device), document_tensor
+        )
+        return torch.max(document_tensor, axis=1).values
+
+    @staticmethod
+    def mean_pooling(attention_mask, document_tensor):
+        mask = attention_mask.unsqueeze(-1).repeat(1, 1, document_tensor.shape[-1])
+        document_tensor = document_tensor * mask
+
+        # Compute sum over the sequence_length axis
+        sum_pooled = document_tensor.sum(dim=1)
+
+        # Compute the count of non-zero mask values for each position
+        non_zero_counts = attention_mask.sum(dim=1, keepdim=True).clamp_min(1e-12)  # Avoid division by zero
+        return sum_pooled / non_zero_counts
+
+    def get_skill_embeddings(self, document, use_jobtitle):
+
+        to_embed = document["skills"]
+        if document["jobtitle"] and use_jobtitle:
+            to_embed = [document["jobtitle"]] + document["skills"]
 
         if self.cache_embeddings:
             skills = []
-            for skill in document["skills"]:
+            for skill in to_embed:
                 if skill in self.cache:
                     skills.append(self.cache[f"{self.skill_prefix}{skill}"])
                 else:
@@ -167,13 +186,13 @@ class Jarvis(nn.Module):
             skills = torch.cat(skills, dim=0)
 
         else:
-            document["skills"] = self.tokenizer(
+            skills = self.tokenizer(
                 document["skills"],
                 max_length=self.max_len,
                 return_tensors='pt',
                 padding="max_length"
             ).data
-            skills = self.base_model(**document["skills"]).last_hidden_state[:, 0]
+            skills = self.base_model(**skills).last_hidden_state[:, 0]
 
         # Pad tensor and get attention masks
         pad_length = self.max_skills
@@ -185,7 +204,8 @@ class Jarvis(nn.Module):
     def forward(self, cv, job, label=None):
 
         cv_emb = self.get_document_embedding(cv, self.ffn_emb_cv, self.ffn_readout_cv)
-        job_emb = self.get_document_embedding(job, self.ffn_emb_job, self.ffn_readout_job)
+        job_emb = self.get_document_embedding(job, self.ffn_emb_job, self.ffn_readout_job,
+                                              use_jobtitle=self.use_jobtitle)
 
         sim = self.cos(cv_emb, job_emb)
         if label is None:
