95a4b499af743fa5eb19c6ab79b66c2e0f77ceb2
bas <bas.krahmer@talentflyxpert.com>
2023-11-17 11:11:54 +0100

    Add CLI option for random Optuna sampler

--- jarvis2/utils/utils.py
+++ jarvis2/utils/utils.py
@@ -97,6 +97,7 @@ def parse_args(args) -> argparse.Namespace:
     parser.add_argument("--score_metric", type=str, required=False, default="val_all_f1")
     parser.add_argument("--lower_is_better", type=bool, required=False, default=False)
     parser.add_argument("--optuna_path", type=str, required=False, default="")
+    parser.add_argument("--random_sampler", type=bool, required=False, default=False)
 
     # Artefact options
     parser.add_argument("--save_path", type=str, required=False, default=Path(__file__).parent.parent.parent / "output")
